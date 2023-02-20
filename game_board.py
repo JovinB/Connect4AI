8ab587dfdbb25eafb8a51630fa146bc8c3b9fb2f
JovinB <jovin.bains007@gmail.com>
2023-02-20 02:46:06 -0500

    Formatted the look of the Tkinter frames to look like an actual Connect4 game :lipstick:

--- game_board.py
+++ game_board.py
@@ -9,13 +9,47 @@ class Board(tk.Tk):
         super().__init__()
         self.game = game
         self.title("Connect4")
+        self.display = None
+        self.create_main_window()
+        self.create_board()
 
     def create_main_window(self):
-        display_frame = tk.Frame(master=self)
-        display_frame.pack(fill=tk.X)
+        main_frame = tk.Frame(master=self)
+        main_frame.pack(fill=tk.X)
+        """
         self.display = tk.Label(
-            master=display_frame,
-            text="Ready?",
-            font=font.Font(size=28, weight="bold"),
+            master=main_frame,
+            # text="Ready?",
+            # font=font.Font(size=28, weight="bold"),
         )
         self.display.pack()
+        """
+
+    def create_board(self):
+        global button_image
+
+        board_frame = tk.Frame(master=self)
+        board_frame.pack()
+        board_frame.config(background="deep sky blue")
+
+        button_image = tk.PhotoImage(file="circle.png")
+
+        for row in range(6):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(7):
+                button = tk.Button(
+                    master=board_frame,
+                    image=button_image,
+                    width=100,
+                    height=100,
+                    borderwidth=0,
+                )
+                # self.tiles[button] = (row, col)
+                # button.bind("<ButtonPress-1>", self.play)
+                button.grid(
+                    row=row,
+                    column=col,
+                    sticky="nsew"
+                )
+
